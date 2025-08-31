9b1953fbf33336a358deafa6fcfcee97163cc871
Evert Appels <evert.app@proton.me>
2025-08-31 17:52:44 +0100

    chore(repo): remove root-level tests and tools for ADR-0001 compliance

--- tests/test_imports_no_cycles.py
+++
@@ -1,8 +0,0 @@
-def test_imports_clean():
-    import importlib
-
-    import bb8_core.bb8_presence_scanner as s
-    import bb8_core.bridge_controller as bc
-
-    importlib.reload(s)
-    importlib.reload(bc)
